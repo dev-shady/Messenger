c5c7675357824661969910565d3e9308fe82d622
ShivamAmitParthUmang <shivam01101995@gmail.com>
2016-02-13 23:14:00 +0530

    Update enterGroup.java
    Use of ServletContext attribute glist to add member in to the new group.

--- src/com/messenger/enterGroup.java
+++ src/com/messenger/enterGroup.java
@@ -9,7 +9,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/enterGroup")
 public class enterGroup extends HttpServlet {
@@ -24,10 +24,15 @@ public class enterGroup extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String groupId=request.getParameter("groupId");
-		fileManagement f=new fileManagement();
-		Cookie[] c=request.getCookies();
+	//	fileManagement f=new fileManagement();
+		String uname="new";
+		GroupList glist=(GroupList)getServletContext().getAttribute("glist");
+		if(glist==null)
+			System.out.println("glist null");
+	Cookie[] c=request.getCookies();
+	//Cookie cookie;
 if(c!=null){
-			
+		System.out.println("cookies not null");
 		if(c[0].getValue().equals(groupId)){
 			status=true;
 		}
@@ -39,6 +44,14 @@ if(c!=null){
 			System.out.println("array out of bounds exception");
 		}
 		if(status){
+			
+			group grp=glist.getGroup(groupId);
+			grp.addMember(uname);
+			grp.addMessage(uname+" joined");
+			HttpSession session=request.getSession();
+			session.setMaxInactiveInterval(60*60);
+			session.setAttribute("uname",uname);
+			session.setAttribute("gname", groupId);
 			response.sendRedirect("chat.jsp");
 		}
 		else{
@@ -51,10 +64,17 @@ if(c!=null){
 	}
 
 else{
-	
-	if(f.fRead(groupId)){
+	System.out.println("cookies null");
+	if(glist.contains(groupId)){							//CHANGED
 	Cookie cs=new Cookie("groupId",groupId);
+	group grp=glist.getGroup(groupId);
+	grp.addMember(uname);
+	grp.addMessage(uname+" joined");
 	response.addCookie(cs);
+	HttpSession session=request.getSession();
+	session.setMaxInactiveInterval(60*60);
+	session.setAttribute("uname",uname);
+	session.setAttribute("gname", groupId);
 	RequestDispatcher rd=request.getRequestDispatcher("chat");
 	rd.forward(request,response);
 	}
