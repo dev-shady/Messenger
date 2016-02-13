18fdada127bddb347f6f6e03c85ec0f360c0cc52
ShivamAmitParthUmang <shivam01101995@gmail.com>
2016-02-13 23:18:14 +0530

    Create Initialiser.java
    Instantiates the GroupList object and save it as the ServletContext. The Servlet loads on web app startup only.

---
+++ src/com/messenger/Initialiser.java
@@ -0,0 +1,52 @@
+package com.messenger;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Initialiser
+ */
+@WebServlet("/Initialiser")
+public class Initialiser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       GroupList glist;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Initialiser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		glist=new GroupList();
+		glist.addGroup(new group("f","first"));
+		getServletContext().setAttribute("glist",glist);
+		System.out.println("Initialiser");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
